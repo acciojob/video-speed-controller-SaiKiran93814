3da1f31ab493c1f87726c27720a40822c5cc591b
SaiKiran93814 <60767499+SaiKiran93814@users.noreply.github.com>
2025-05-20 12:18:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.viewer');
+const toggle = document.querySelector('.toggle');
+const skipButtons = document.querySelectorAll('[data-skip]');
+const sliders = document.querySelectorAll('.player__slider');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+// Toggle video play/pause
+function togglePlay() {
+  video.paused ? video.play() : video.pause();
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+// Update the play/pause button icon
+function updateButton() {
+  toggle.textContent = video.paused ? '►' : '❚ ❚';
+}
+
+// Skip forward/backward
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+// Handle volume and playbackRate
+function handleSliderUpdate() {
+  video[this.name] = this.value;
+}
+
+// Update the progress bar
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+// Scrub to different position in video
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+// Event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+sliders.forEach(slider => slider.addEventListener('input', handleSliderUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
